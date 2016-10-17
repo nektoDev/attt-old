7b59309202d0018aae65a0f567c30ead29f4d658
nektodev <ts.slawa@gmail.com>
2016-10-17 09:49:39 +0300

    Complete migration to ApacheHttp

--- src/main/java/ru/nektodev/service/attt/service/TransmissionService.java
+++ src/main/java/ru/nektodev/service/attt/service/TransmissionService.java
@@ -3,12 +3,11 @@ package ru.nektodev.service.attt.service;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.stereotype.Service;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -20,34 +19,29 @@ public class TransmissionService {
 
 
     public static final String TRANSMISSION_URL = "http://192.168.1.11:9091/transmission/rpc";
+    private HttpClient client;
 
     public TransmissionService() {
+        client = new DefaultHttpClient();
     }
 
     public boolean addToTransmission(String downloadDir, String magnet) throws IOException {
         URL obj = new URL(TRANSMISSION_URL);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        con.setRequestMethod("POST");
-        String session = getSession();
-        System.out.println(session);
-        con.addRequestProperty("X-Transmission-Session-Id", session);
-
-        String urlParameters = "{\n" +
+        HttpPost post = new HttpPost(TRANSMISSION_URL);
+        post.addHeader("X-Transmission-Session-Id", getSession());
+        String body = "{\n" +
                 "\t\"method\" : \"torrent-add\",\n" +
                 "\t\"arguments\" : {\n" +
                 "\t\t\"filename\":\"" + magnet + "\",\n" +
                 "\t\t\"download-dir\": \""+ downloadDir +"\" \n" +
                 "\t}\n" +
                 "}";
-        System.out.println(urlParameters);
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
-        wr.flush();
-        wr.close();
+        StringEntity params = new StringEntity(body);
+        post.setEntity(params);
+        HttpResponse response = client.execute(post);
 
-        int responseCode = con.getResponseCode();
+        int responseCode = response.getStatusLine().getStatusCode();
 
         System.out.println("[" + responseCode + "]");
 
@@ -55,14 +49,8 @@ public class TransmissionService {
     }
 
     private String getSession() throws IOException {
-
-        HttpClient client = new DefaultHttpClient();
         HttpPost post = new HttpPost(TRANSMISSION_URL);
-
         HttpResponse response = client.execute(post);
-        System.out.println("Response Code : " +
-                response.getStatusLine().getStatusCode());
-
         return response.getFirstHeader("X-Transmission-Session-Id").getValue();
     }
 }
