94d2f7568ad4d443929bf606a6e215f2a9808533
nektodev <ts.slawa@gmail.com>
2016-11-03 08:06:29 +0300

    Change model

--- src/main/java/ru/nektodev/service/attt/model/TorrentInfo.java
+++ src/main/java/ru/nektodev/service/attt/model/TorrentInfo.java
@@ -3,6 +3,8 @@ package ru.nektodev.service.attt.model;
 import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author nektodev
@@ -10,10 +12,17 @@ import java.io.Serializable;
  */
 public class TorrentInfo implements Serializable {
     @Id
+    private String id;
+    private String name;
     private String url;
     private String magnet;
     private String downloadDir;
-    private String name;
+    private boolean autoDownload;
+
+    private List<String> watchers;
+
+    private Date added;
+    private Date finished;
 
     public String getUrl() {
         return url;
@@ -41,5 +50,49 @@ public class TorrentInfo implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAutoDownload() {
+        return autoDownload;
+    }
+
+    public void setAutoDownload(boolean autoDownload) {
+        this.autoDownload = autoDownload;
+    }
+
+    public List<String> getWatchers() {
+        return watchers;
+    }
+
+    public void setWatchers(List<String> watchers) {
+        this.watchers = watchers;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getAdded() {
+        return added;
+    }
+
+    public void setAdded(Date added) {
+        this.added = added;
+    }
+
+    public Date getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Date finished) {
+        this.finished = finished;
     }
 }
